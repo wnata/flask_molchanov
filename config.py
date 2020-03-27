fc0dd0cdb775fc770ab6faa39a43345ad939417f
winston <wnatakillemall@gmail.com>
2020-03-27 13:19:33 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://newuser:Smith&Wessen1323@localhost/test1"
---
+++ posts/blueprint.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from flask import render_template
+
+from models import Post, Tag
+
+posts = Blueprint('posts', __name__, template_folder='templates')
+
+
+@posts.route('/')
+def index():
+    posts = Post.query.all()
+
+    return render_template('posts/index.html', posts=posts)
+
+
+@posts.route('/<slug>')
+def post_detail(slug):
+    post = Post.query.filter(Post.slug==slug).first()
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
